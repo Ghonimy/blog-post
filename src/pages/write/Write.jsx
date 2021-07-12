56d51f97ee96eccf2115d225e034965c7ecee394
Ghonimy <ghonimy444@hotmail.com>
2021-07-12 23:15:55 +0200

    remove the proxy in package-json

--- src/pages/write/Write.jsx
+++ src/pages/write/Write.jsx
@@ -23,7 +23,7 @@ export default function Write() {
       data.append("file", file);
       newPost.photo = filename;
       try {
-        await axios.post("/upload", data);
+        await axios.post("https://ghonemy-blog.herokuapp.com/api/upload", data);
       } catch (err) {}
     }
     try {
